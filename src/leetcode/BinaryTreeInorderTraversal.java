0ba3887960643576ba9d82c4213fdac1e8f9cb6f
hmilycsh <pingzizzp@163.com>
2019-04-18 00:23:22 +0800

    postordertraversal

--- src/leetcode/BinaryTreeInorderTraversal.java
+++ src/leetcode/BinaryTreeInorderTraversal.java
@@ -45,4 +45,18 @@ public class BinaryTreeInorderTraversal {
 		return list;
 		
 	}
+	
+	public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root,list);
+        return list;   
+    }
+    
+    private void helper(TreeNode root,List<Integer> list){
+        if(root!=null){
+            helper(root.left,list);
+            helper(root.right,list);
+            list.add(root.val);
+        }
+    }
 }
